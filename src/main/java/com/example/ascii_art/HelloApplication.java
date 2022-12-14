9ee4ea4f6df81cdbd329bc2bec6de89a6f0fafce
psamatheee <juliaxxee@gmail.com>
2022-12-14 16:35:58 +0000

    Added Save button

--- src/main/java/com/example/ascii_art/HelloApplication.java
+++ src/main/java/com/example/ascii_art/HelloApplication.java
@@ -24,7 +24,9 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 
 public class HelloApplication extends Application {
@@ -36,12 +38,11 @@ public class HelloApplication extends Application {
         public TextSizeTransition(Text text, int start, int end, Duration duration) {
             this.text = text;
             this.start = start;
-            this.end = start - end; // minus start because of (end * frac) + start in interpolate()
+            this.end = start - end;
             setCycleDuration(duration);
             setInterpolator(Interpolator.LINEAR);
             setCycleCount(1);
         }
-
 
         @Override
         protected void interpolate(double v) {
@@ -50,65 +51,20 @@ public class HelloApplication extends Application {
                 text.setFont(Font.font("Monospaced", size));
             }
             if(size < start - end){
-
                 text.setFont(Font.font("Monospaced", start - end));
             }
         }
     }
+
     @Override
     public void start(Stage stage) throws IOException {
         double width = Screen.getPrimary().getBounds().getWidth();
         double height = Screen.getPrimary().getBounds().getHeight();
-        VBox root = new VBox();
-        root.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-        //elements
-        Text text = new Text("ASCII ART GENERATOR");
-        text.setFont(Font.font("Monospaced", 80));
-        text.setFill(Color.WHITE);
-        Button uploadButton = new Button("UPLOAD IMAGE");
-        uploadButton.setStyle("-fx-background-color: white; -fx-font: 20px \"Monospaced\";");
-        root.getChildren().addAll(text,uploadButton);
-        root.setAlignment(Pos.CENTER);
-        root.setSpacing(40);
+        VBox root = getStartWindow(stage, width, height);
+
         Scene scene = new Scene(root, width, height);
         stage.setTitle("ASCII ART");
         stage.setScene(scene);
-
-        //Control
-        uploadButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                FileChooser fileChooser = new FileChooser();
-                FileChooser.ExtensionFilter jpgext = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.jpg", "*.JPG");
-                fileChooser.getExtensionFilters().add(jpgext);
-                File file = fileChooser.showOpenDialog(null);
-
-                if (file != null) {
-
-                    String ascii = getAsciiText(file,width,height);
-                    StackPane window = new StackPane();
-
-                    window.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-
-                    Text text1 = new Text(ascii);
-
-
-                    text1.setTextAlignment(TextAlignment.CENTER);
-                    text1.setFont(Font.font("Monospaced", 10));
-                    text1.setFill(Color.WHITE);
-
-                    window.getChildren().add(text1);
-                    window.setAlignment(Pos.CENTER);
-                    TextSizeTransition trans = new TextSizeTransition(text1, 40, 3,Duration.millis(5000));
-                    stage.getScene().setRoot(window);
-                    stage.getScene().setFill(Color.BLACK);
-                    stage.show();
-                    trans.play();
-                }
-            }
-        });
-
-
         stage.show();
     }
 
@@ -117,97 +73,39 @@ public class HelloApplication extends Application {
     }
 
     public static String getAsciiText(File file, double width, double height){
-        Image image = new Image(file.toURI().toString(),width/4,height/4,true,false);
+        Image image = new Image(file.toURI().toString(),width/4,height/4 - 40 ,true,false);
         PixelReader pixelReader = image.getPixelReader();
         String scale = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-        int mapping = 100/scale.length();
         StringBuilder ascii = new StringBuilder();
         for(int i = 0; i < image.getHeight(); i++){
             for(int j = 0; j < image.getWidth(); j++){
                 Color col = pixelReader.getColor(j,i);
                 double val = 0.3 * col.getRed() + 0.59 * col.getGreen() + 0.11 * col.getBlue();
                 int selector = (int) (val * scale.length());
-                ascii.append((""+scale.charAt(selector)).repeat(3));
+                ascii.append((""+scale.charAt(selector)).repeat(2)); // font about 2 times tall as wide
             }
             ascii.append("\n");
         }
-        return ascii.toString();package com.example.ascii_art;
-
-import javafx.animation.*;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Labeled;
-import javafx.scene.image.*;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.stage.FileChooser;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-
-import java.io.File;
-import java.io.IOException;
-
-
-public class HelloApplication extends Application {
-    public class TextSizeTransition extends Transition {
-
-        private Text text; // a little generic -> subclasses: ButtonBase, Cell, Label, TitledPane
-        private int start, end; // initial and final size of the text
-
-        public TextSizeTransition(Text text, int start, int end, Duration duration) {
-            this.text = text;
-            this.start = start;
-            this.end = start - end; // minus start because of (end * frac) + start in interpolate()
-            setCycleDuration(duration);
-            setInterpolator(Interpolator.LINEAR);
-            setCycleCount(1);
-        }
-
-
-        @Override
-        protected void interpolate(double v) {
-            int size = (int) (start - (end * v));
-            if(size>= start - end) {
-                text.setFont(Font.font("Monospaced", size));
-            }
-            if(size < start - end){
-
-                text.setFont(Font.font("Monospaced", start - end));
-            }
-        }
+        return ascii.toString();
     }
-    @Override
-    public void start(Stage stage) throws IOException {
-        double width = Screen.getPrimary().getBounds().getWidth();
-        double height = Screen.getPrimary().getBounds().getHeight();
+
+    public VBox getStartWindow(Stage stage, double width, double height){
         VBox root = new VBox();
         root.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-        //elements
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(40);
+
+        //Text
         Text text = new Text("ASCII ART GENERATOR");
         text.setFont(Font.font("Monospaced", 80));
         text.setFill(Color.WHITE);
+
+        //Start Button
         Button uploadButton = new Button("UPLOAD IMAGE");
         uploadButton.setStyle("-fx-background-color: white; -fx-font: 20px \"Monospaced\";");
-        root.getChildren().addAll(text,uploadButton);
-        root.setAlignment(Pos.CENTER);
-        root.setSpacing(40);
-        Scene scene = new Scene(root, width, height);
-        stage.setTitle("ASCII ART");
-        stage.setScene(scene);
 
-        //Control
+
+        root.getChildren().addAll(text,uploadButton);
         uploadButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
@@ -217,19 +115,17 @@ public class HelloApplication extends Application {
                 File file = fileChooser.showOpenDialog(null);
 
                 if (file != null) {
-
                     String ascii = getAsciiText(file,width,height);
-                    GridPane window = new GridPane();
-
+                    BorderPane window  = new BorderPane();
                     window.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
 
-                    Text text1 = new Text(ascii);
+                    Text asciiText = new Text(ascii);
+                    asciiText.setTextAlignment(TextAlignment.CENTER);
+                    asciiText.setFont(Font.font("Monospaced", 10));
+                    asciiText.setFill(Color.WHITE);
 
-
-                    text1.setTextAlignment(TextAlignment.CENTER);
-                    text1.setFont(Font.font("Monospaced", 10));
-                    text1.setFill(Color.WHITE);
-                    Button backButton = new Button("Upload Another");
+                    //Buttons
+                    Button backButton = new Button("BACK");
                     backButton.setStyle("-fx-background-color: white; -fx-font: 15px \"Monospaced\";");
                     backButton.setOnAction(new EventHandler<ActionEvent>() {
                         @Override
@@ -237,47 +133,48 @@ public class HelloApplication extends Application {
                             stage.getScene().setRoot(root);
                         }
                     });
-                    window.add(text1,0,0);
-                    window.setAlignment(Pos.CENTER);
-                    TextSizeTransition trans = new TextSizeTransition(text1, 40, 3,Duration.millis(5000));
+
+                    Button saveButton = new Button("SAVE");
+                    saveButton.setStyle("-fx-background-color: white; -fx-font: 15px \"Monospaced\";");
+                    saveButton.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent actionEvent) {
+                            try {
+                                FileWriter fileWriter = new FileWriter(file.getName() + "_ascii.txt", false);
+                                fileWriter.write(ascii);
+                                fileWriter.close();
+                            } catch (IOException e) {
+                                System.out.println("An error occurred.");
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                    StackPane textPane = new StackPane();
+                    textPane.setAlignment(Pos.CENTER);
+                    textPane.getChildren().add(asciiText);
+                    textPane.setPadding(new Insets(20,20,20,20));
+
+                    HBox buttonPane = new HBox();
+                    buttonPane.getChildren().addAll(backButton,saveButton);
+                    buttonPane.setAlignment(Pos.CENTER);
+                    buttonPane.setSpacing(20);
+                    buttonPane.setPadding(new Insets(20,20,20,20));
+
+                    window.setBottom(buttonPane);
+                    window.setCenter(textPane);
+
+                    TextSizeTransition trans = new TextSizeTransition(asciiText, 40, 3,Duration.millis(5000));
                     stage.getScene().setRoot(window);
                     stage.getScene().setFill(Color.BLACK);
-                    //stage.show();
+
                     trans.play();
-                    window.add(backButton,0,1);
                 }
             }
         });
-
-
-        stage.show();
+return root;
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
-
-    public static String getAsciiText(File file, double width, double height){
-        Image image = new Image(file.toURI().toString(),width/16,height/4 - 40,true,false);
-        PixelReader pixelReader = image.getPixelReader();
-        String scale = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-        int mapping = 100/scale.length();
-        StringBuilder ascii = new StringBuilder();
-        for(int i = 0; i < image.getHeight(); i++){
-            for(int j = 0; j < image.getWidth(); j++){
-                Color col = pixelReader.getColor(j,i);
-                double val = 0.3 * col.getRed() + 0.59 * col.getGreen() + 0.11 * col.getBlue();
-                int selector = (int) (val * scale.length());
-                ascii.append((""+scale.charAt(selector)).repeat(3));
-            }
-            ascii.append("\n");
-        }
-        return ascii.toString();
-    }
-
-    public static VBox getMainScene(){
-        return null;
-    }
 }
-    }
-}
+
+
